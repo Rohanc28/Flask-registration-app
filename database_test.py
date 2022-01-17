537929ad3b8e60f682a91a5e874eb32f84118a82
Rohanc28 <81807980+Rohanc28@users.noreply.github.com>
2022-01-17 20:54:29 +0530

    Update database_test.py

--- database_test.py
+++ database_test.py
@@ -5,9 +5,9 @@ menu_prompt = """\n\n--- Sports Tournament ---
 Welcome to Sports Tournament, please register if you have not!
 
 1) Register
-2) Find registrant(s) detail by name
-3) Find registrant(s) for specific sport
-4) See all registrant(s)
+2) Find registrant detail by name
+3) Find registratns for specific sport
+4) See all registrants
 5) Deregister User
 6) Exit
 
@@ -21,7 +21,11 @@ def menu():
 
     while (user_input := input(menu_prompt)) != "6":
         print(user_input)
-        if user_input == "1":  # register
+
+# register
+
+
+        if user_input == "1":
             first_name = input("Enter First name\n")
             while(first_name.isdigit()):
                 print("Invalid name\n")
@@ -43,27 +47,35 @@ def menu():
 
             print("You have successfully registered!\n\n")
 
-        elif user_input == "2":  # Find registrant detail by name
+# Find registrant detail by name
+
+        elif user_input == "2":
             first_name = input("Enter first name\n")
             last_name = input("Enter last name\n")
             u = db.get_user_name(connection, first_name, last_name)
             for _ in u:
-                print(f"\t{_[1]} {_[2]}\t \t- \t{_[3]}\t,\t {_[4]}")
+                print(f"\t{_[0]} {_[1]}\t \t- \t{_[2]}\t,\t {_[3]}")
 
-        elif user_input == "3":  # Find registratns for specific sport
+# Find registratns for specific sport
+
+        elif user_input == "3":
             print("Select sport: \n")
             print(sports)
             sport = input("")
             u = db.sport_list(connection, sport)
             for _ in u:
-                print(f"\t{_[1]} {_[2]}\t \t- \t{_[3]}\t,\t {_[4]}")
+                print(f"\t{_[0]} {_[1]}\t \t- \t{_[2]}\t,\t {_[3]}")
 
-        elif user_input == "4":  # See all registrants
+# See all registrants
+
+        elif user_input == "4":
             u = db.get_all(connection)
             for _ in u:
-                print(f"\t{_[1]} {_[2]}\t \t- \t{_[3]}\t,\t {_[4]}")
+                print(f"\t{_[0]} {_[1]}\t \t- \t{_[2]}\t,\t {_[3]}")
 
-        elif user_input == "5":  # Deregister user by name
+# Deregister user by name
+
+        elif user_input == "5":
             first_name = input("Enter first name of user to deregister\n")
             while first_name == "":
                 print("Invalid Name\n")
